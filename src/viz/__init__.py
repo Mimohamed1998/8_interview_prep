153baddf532b44cf69398446f010f2d170657947
mimohamed1998 <mohamedinasmohamed1998@gmail.com>
2026-05-23 13:22:45 +0530

    initial changes on the repo has been made

--- src/viz/__init__.py
+++
@@ -1,7 +0,0 @@
-from .mckinsey_theme import (
-    BG_DARK, GRID_COLOR, TEXT_WHITE, TEXT_DARK,
-    LINE_NOMINAL, LINE_VOLUME, LINE_REAL, LINE_TREND, ARROW_COLOR, HIGHLIGHT,
-    ANNOTATION_BG, MCKINSEY_RC, DEFAULT_LINE_COLORS,
-    apply_mckinsey_theme, mckinsey_style, create_mckinsey_figure,
-    set_mckinsey_color_cycle, add_oval_annotation, add_end_label, add_bracket_arrow,
-)
